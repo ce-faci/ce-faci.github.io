7f43d395450cbd69c559bac354024b12ba624fcc
thereisnothingimpossible <45714998+thereisnothingimpossible@users.noreply.github.com>
2019-02-11 23:03:51 +0300

    Add files via upload

--- js/preloader.js
+++ js/preloader.js
@@ -1,12 +1,13 @@
 $(window).on( "load", function() {
         if ($('#page-preloader').addClass('done')) {
-        	$('#page-preloader').removeClass('done');
+        	setTimeout(function() {
+        		$('#page-preloader').removeClass('done');
+        	}, 500);
         	setTimeout(function() {
         		$('#page-preloader').addClass('done');
         	}, 2000);
         	setTimeout(function() {
         		$('.preloader').addClass('done');
-        		$('#hide').removeClass('hide');
         	}, 3000);
         }
     });
