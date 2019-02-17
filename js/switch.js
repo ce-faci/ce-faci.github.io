193a888552ec5d8cece7b48b0df8bac82d66af09
thereisnothingimpossible <45714998+thereisnothingimpossible@users.noreply.github.com>
2019-02-17 14:01:16 +0300

    Add files via upload

--- js/switch.js
+++ js/switch.js
@@ -7,18 +7,18 @@ var vkComments = document.getElementById('vk_comments');
 
 checkbox.addEventListener('change', function () {
 if (checkbox.checked) {
-vk.style.opacity = '0.5';
-fb.style.opacity = '1';
-vk.style.transition = '0.5s';
-fb.style.transition = '0.5s';
-vkComments.style.display = 'none';
-fbComments.style.display = 'initial';
-console.log('Checked');
-} else {
 vk.style.opacity = '1';
 fb.style.opacity = '0.5';
+vk.style.transition = '0.5s';
+fb.style.transition = '0.5s';
 vkComments.style.display = 'initial';
 fbComments.style.display = 'none';
+console.log('Checked');
+} else {
+vk.style.opacity = '0.5';
+fb.style.opacity = '1';
+vkComments.style.display = 'none';
+fbComments.style.display = 'initial';
 console.log('Not checked');
 }
 });
